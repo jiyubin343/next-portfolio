742faf398a3e7ae2bb3993ed4fe668d2d3f09361
jiyubin343 <gdang343@gmail.com>
2024-08-26 20:13:54 +0900

    8_26

--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,13 @@ export default function Home() {
   return (
     <Layout>
       <Head>
-        <title>Create Next App</title>
+        <title>유빈의 포트폴리오</title>
         <meta name="description" content="나의 포트폴리오" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>포트폴리오</h1>
+      <h1>유빈의 포트폴리오</h1>
       </Layout>
     );
 }      
